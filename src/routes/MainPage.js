c87bbb9d2e8143a1b3ad6ef6134a7dae2ecf52c0
Ricardo Portatil UBUNTU <ricardovillanueva282@gmail.com>
2023-02-23 17:12:05 +0100

    Home listo, problema con el background

--- src/routes/MainPage.js
+++ src/routes/MainPage.js
@@ -1,9 +1,16 @@
-import '../componentStyles/mainP.css';
+import '../componentStyles/mainP.css'
 
 export default function MainPage(){
+    const gohome = () =>{
+        window.location.replace("/home");
+    }
     return(
-        <div>
-            <h1>MainPage</h1>
+       
+        <div id="mainPage">
+            <h1>WikiBeer</h1>
+            <p>Bienvenido a WikiBeer, una página para la consulta y disfrute de el gran mundo de las cervezas. Una exploración por el amplio catalogo de tipos!!</p>
+            <button onClick={gohome}>¡Iniciar!</button>
         </div>
+    
     )
 }
