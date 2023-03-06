560b5c710c19630b08d2fe2dbc0049c0ad4c1745
Ricardo <ricardovillanueva282@gmail.com>
2023-03-06 12:27:11 +0100

    Sobremi

--- src/routes/Aboutus.js
+++ src/routes/Aboutus.js
@@ -13,8 +13,22 @@ export default function AboutUs() {
                 <article>
                     <h1>Ricardo Villanueva Alhama</h1>
                     <img src={rikipera} alt="rikipera" id="rikipera"></img>
+                    <p>
+                        ¡Hola a todos! Mi nombre es Ricardo Villanueva Alhama, tengo 20 años y actualmente estoy estudiando un grado superior en Desarrollo de Aplicaciones Web. Anteriormente, trabajé como técnico informático en el sector hospitalario y también en el mundo de la moda. Siendo un apasionado por los trabajos de cara al público o que tengan un trato cercano al cliente, estoy en búsqueda activa de nuevas oportunidades para seguir desarrollándome en estas áreas.
+                    </p>
+                    <p>
+                        Como colaborador en este proyecto sobre catas de cervezas, estoy emocionado de poder compartir mi experiencia y conocimientos con todos ustedes. Mi experiencia en el sector informático me ha brindado habilidades técnicas valiosas que me permiten comprender mejor los desafíos y oportunidades de la industria cervecera, y 0 ha enseñado la importancia del trato cercano y personalizado al cliente.
+                    </p>
+
+                    <p>
+                        Estoy emocionado por la oportunidad de trabajar junto a un equipo apasionado y comprometido en este proyecto. Espero poder contribuir a crear una experiencia única para los amantes de la cerveza y ayudar a crear conexiones significativas con los clientes a través de este proyecto.
+                    </p>
+
+                    <p>
+                        ¡Gracias por considerar mi perfil y espero poder conocer a todos ustedes pronto en las catas de cerveza!
+                    </p>
                 </article>
             </section>
         </div>
-    )
+    )   
 }
