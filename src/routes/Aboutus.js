bd804b643189830df10cddf3451cc432053a6497
Ricardo <ricardovillanueva282@gmail.com>
2023-03-06 10:50:21 +0100

    RikiPera

--- src/routes/Aboutus.js
+++ src/routes/Aboutus.js
@@ -4,6 +4,17 @@ export default function AboutUs() {
     return(
         <div>
             <Navbar></Navbar>
+
+            <section id="aboutusContainer">
+                <article>
+                    <p>Hugo things</p>
+                </article>
+
+                <article>
+                    
+
+                </article>
+            </section>
         </div>
     )
 }
