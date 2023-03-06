b3734c2d99f6bc94b171a11df4da850ab6671a92
Ricardo <ricardovillanueva282@gmail.com>
2023-03-06 11:53:29 +0100

    Push hugo

--- src/routes/Aboutus.js
+++ src/routes/Aboutus.js
@@ -1,18 +1,18 @@
 import Navbar from "./Navbar";
-
+import rikipera from "../img/rikipera.png"
 export default function AboutUs() {
     return(
         <div>
             <Navbar></Navbar>
 
-            <section id="aboutusContainer">
+            <section class="ContainerAv">
                 <article>
                     <p>Hugo things</p>
                 </article>
-
+                <hr></hr>
                 <article>
-                    
-
+                    <h1>Ricardo Villanueva Alhama</h1>
+                    <img src={rikipera} alt="rikipera" id="rikipera"></img>
                 </article>
             </section>
         </div>
